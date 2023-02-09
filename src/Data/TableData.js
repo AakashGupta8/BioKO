0c1db61795b1f796b9611c325dbcd57900d68462
Shilpee-Agrawal <101170944+Shilpee-Agrawal@users.noreply.github.com>
2023-02-09 16:03:24 +0530

    Data added

---
+++ src/Data/TableData.js
@@ -0,0 +1,196 @@
+import Image1 from "../Images/Abbildung6.png";
+import Image2 from "../Images/Abbildung5.png";
+import Image3 from "../Images/Abbildung7.png";
+
+const Data = [
+    {
+        id: "101",
+        img: Image1,
+        result:"Negative",
+        img_name:"Bild1",
+        img_description:"Beschreibung1",
+
+    }, {
+        id: "102",
+        img: Image2,
+        result:"Positive",
+        img_name:"Bild2",
+        img_description:"Beschreibung2",
+    }, {
+        id: "103",
+        img: Image2,
+        result:"Positive",
+        img_name:"Bild3",
+        img_description:"Beschreibung3",
+    }, {
+        id: "104",
+        img: Image3,
+        result:"Positive",
+        img_name:"Bild4",
+        img_description:"Beschreibung4",
+    }, {
+        id: "105",
+        img: Image3,
+        result:"Positive",
+        img_name:"Bild5",
+        img_description:"Beschreibung5",
+    }, {
+        id: "106",
+        img: Image3,
+        result:"Positive",
+        img_name:"Bild6",
+        img_description:"Beschreibung6",
+    },{
+        id: "107",
+        img: Image2,
+        result:"Positive",
+        img_name:"Bild7",
+        img_description:"Beschreibung7",
+    }, {
+        id: "108",
+        img: Image2,
+        result:"Positive",
+        img_name:"Bild8",
+        img_description:"Beschreibung8",
+    }, {
+        id: "109",
+        img: Image1,
+        result:"Negative",
+        img_name:"Bild9",
+        img_description:"Beschreibung9",
+    }, {
+        id: "110",
+        img: Image1,
+        result:"Negative",
+        img_name:"Bild10",
+        img_description:"Beschreibung10",
+    },
+
+    {
+        id: "201",
+        img: Image1,
+        result:"Negative",
+        img_name:"Bild11",
+        img_description:"Beschreibung11",
+
+    }, {
+        id: "202",
+        img: Image2,
+        result:"Positive",
+        img_name:"Bild12",
+        img_description:"Beschreibung12",
+    }, {
+        id: "203",
+        img: Image2,
+        result:"Positive",
+        img_name:"Bild13",
+        img_description:"Beschreibung13",
+    }, {
+        id: "204",
+        img: Image3,
+        result:"Positive",
+        img_name:"Bild14",
+        img_description:"Beschreibung14",
+    }, {
+        id: "205",
+        img: Image3,
+        result:"Positive",
+        img_name:"Bild15",
+        img_description:"Beschreibung15",
+    }, {
+        id: "206",
+        img: Image3,
+        result:"Positive",
+        img_name:"Bild16",
+        img_description:"Beschreibung16",
+    },{
+        id: "207",
+        img: Image2,
+        result:"Positive",
+        img_name:"Bild17",
+        img_description:"Beschreibung17",
+    }, {
+        id: "208",
+        img: Image2,
+        result:"Positive",
+        img_name:"Bild18",
+        img_description:"Beschreibung18",
+    }, {
+        id: "209",
+        img: Image1,
+        result:"Negative",
+        img_name:"Bild19",
+        img_description:"Beschreibung19",
+    }, {
+        id: "210",
+        img: Image1,
+        result:"Negative",
+        img_name:"Bild20",
+        img_description:"Beschreibung20",
+    },
+
+    {
+        id: "301",
+        img: Image1,
+        result:"Negative",
+        img_name:"Bild21",
+        img_description:"Beschreibung21",
+
+    }, {
+        id: "302",
+        img: Image2,
+        result:"Positive",
+        img_name:"Bild22",
+        img_description:"Beschreibung22",
+    }, {
+        id: "303",
+        img: Image2,
+        result:"Positive",
+        img_name:"Bild23",
+        img_description:"Beschreibung23",
+    }, {
+        id: "304",
+        img: Image3,
+        result:"Positive",
+        img_name:"Bild24",
+        img_description:"Beschreibung24",
+    }, {
+        id: "305",
+        img: Image3,
+        result:"Positive",
+        img_name:"Bild25",
+        img_description:"Beschreibung25",
+    }, {
+        id: "306",
+        img: Image3,
+        result:"Positive",
+        img_name:"Bild26",
+        img_description:"Beschreibung26",
+    },{
+        id: "307",
+        img: Image2,
+        result:"Positive",
+        img_name:"Bild27",
+        img_description:"Beschreibung27",
+    }, {
+        id: "308",
+        img: Image2,
+        result:"Positive",
+        img_name:"Bild28",
+        img_description:"Beschreibung28",
+    }, {
+        id: "309",
+        img: Image1,
+        result:"Negative",
+        img_name:"Bild29",
+        img_description:"Beschreibung29",
+    }, {
+        id: "310",
+        img: Image1,
+        result:"Negative",
+        img_name:"Bild30",
+        img_description:"Beschreibung30",
+    }
+]
+
+export default Data
